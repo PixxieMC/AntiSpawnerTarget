6f014b924fada4512073b8717ea8afa15f0739b4
BuildTools <erikhampus@live.se>
2020-02-05 11:15:06 +0100

    Fixed not 100% cancel bug.

--- src/main/java/net/pixxy/antispawnertarget/AntiSpawnerTarget.java
+++ src/main/java/net/pixxy/antispawnertarget/AntiSpawnerTarget.java
@@ -3,10 +3,9 @@ package net.pixxy.antispawnertarget;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityTargetEvent;
-import org.bukkit.event.entity.SpawnerSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -18,14 +17,17 @@ public final class AntiSpawnerTarget extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onEntitySpawn(SpawnerSpawnEvent e) {
-        e.getEntity().setSilent(true);
-        e.getEntity().setMetadata("spawner-spawn",
-            new FixedMetadataValue(this, "true"));
+    public void onCreatureSpawn(CreatureSpawnEvent e) {
+        if (e.getSpawnReason().equals(CreatureSpawnEvent.SpawnReason.SPAWNER) ||
+            e.getSpawnReason().equals(CreatureSpawnEvent.SpawnReason.CUSTOM)) {
+            e.getEntity().setMetadata("spawner-spawn",
+                new FixedMetadataValue(this, true));
+            e.getEntity().setSilent(true);
+        }
     }
 
     @EventHandler
-    public void onEntityTarget(EntityTargetEvent e) {
+    public void onEntityTarget(EntityTargetLivingEntityEvent e) {
         if (e.getEntity().hasMetadata("spawner-spawn") &&
             e.getTarget() instanceof Player) {
             e.setCancelled(true);
